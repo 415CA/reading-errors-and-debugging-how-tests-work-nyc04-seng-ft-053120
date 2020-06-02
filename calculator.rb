c2198af462c8346e737257ec0e7e45bd88298bb9
Chris Allen <chrisallen415@gmail.com>
2020-06-02 15:05:39 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = num1
-second_number = num2
+first_number = 1
+second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
